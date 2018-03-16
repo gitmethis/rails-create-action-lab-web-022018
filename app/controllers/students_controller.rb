cc5ac1b5b7af95ebfb858b2558af18181e8e2e66
Chase <chaseit12@gmail.com>
2018-03-16 19:12:48 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,7 +15,7 @@ class StudentsController < ApplicationController
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
-    redirect_to post_path(@post)
+    redirect_to post_path(@student)
   end
 
 end
